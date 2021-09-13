27a6e9b7f19ef65bfa03661f96ef50deeb6984bc
CarterWurst2023 <816568@mystma.org>
2021-09-13 09:39:35 -0500

    1st lesson

---
+++ main.py
@@ -0,0 +1,14 @@
+#print first movie
+print("spooderman far from home")
+
+#print 2nd movie
+print("into the spider verse")
+
+#print 3rd movie
+print("gardians of the galaxy 1")
+
+#print 3rd movie
+print("gardians of the galaxy 2")
+
+#print 3rd movie
+print("gardians of the galaxy 3")
