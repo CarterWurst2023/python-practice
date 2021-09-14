592e950c8dada9ed1187213203dd33330797d283
CarterWurst2023 <816568@mystma.org>
2021-09-14 09:33:19 -0500

    betterer

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-#print first movie
-print("spooderman far from home")
+def topFiveMovies():
+    #print the first movie
+    print("spooderman into the spooderverse")
 
-#print 2nd movie
-print("into the spider verse")
+    #print second movie
+    print("gardians of the galaxy 2")
 
-#print 3rd movie
-print("gardians of the galaxy 1")
+    #print third movie
+    print("the 5th starwars movie")
 
-#print 4th movie
-print("gardians of the galaxy 2")
+    #print 4th movie
+    print("spooderman homcoming")
 
-#print 5th movie
-print("cars 8")
+    #print 5th movie
+    print("cars 7")
+
+print("these are my top 5 movies")
+topFiveMovies()
