41796dea85d7f03c352e923d4dd5b24c6392f76c
CarterWurst2023 <816568@mystma.org>
2021-09-15 09:48:44 -0500

    function parameters

--- main.py
+++ main.py
@@ -1,44 +1,8 @@
+def myNameis(tortuga):
 
-def lightningMcqueensays():
-    print("kachow")
-    print("i am speeed")
+    print(tortuga + " is my name")
 
-def eAsports():
-    print("EA SPORTS")
-    print("its in the game")
-
-def  favSports():
-    #print 1st sport
-    print("cross country")
-    #print second sport
-    print("swimming")
-    #print 3rd sport
-    print("hockey")
-
-
-def topFiveMovies():
-    #print the first movie
-    print("spooderman into the spooderverse")
-
-    #print second movie
-    print("gardians of the galaxy 2")
-
-    #print third movie
-    print("the 5th starwars movie")
-
-    #print 4th movie
-    print("spooderman homcoming")
-
-    #print 5th movie
-    print("cars 9")
-
-print("these are my top 5 movies")
-topFiveMovies()
-
-eAsports()
-
-print("lightning mcqueen be like")
-lightningMcqueensays()
-
-print("top three favorite sports")
-favSports()
+myNameis("carter")
+myNameis("creter")
+myNameis("crater")
+myNameis("chortor")
